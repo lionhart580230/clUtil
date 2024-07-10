1b72d5f36f0488aaff84add801896085550a17aa
xiaolan580230 <xiaolan580230@gmail.com>
2024-07-10 14:07:12 +0800

    修复获取用户信息

--- clWeixin/getUserInfo.go
+++ clWeixin/getUserInfo.go
@@ -21,16 +21,16 @@ type WXRespUserInfo struct {
 	UnionId    string   `json:"unionid"`
 }
 
-func GetUserInfo(_appId string, _appSecret string, _openId string) (error, *WXRespUserInfo) {
-
-	accessToken, err := GetAccessToken(_appId, _appSecret)
+// 获取用户信息
+func GetUserInfo(_appId string, _appSecret string, _code string) (error, *WXRespUserInfo) {
+	err, accessTokenInfo := UserCodeToAccessToken(_appId, _appSecret, _code)
 	if err != nil {
 		return err, nil
 	}
-	
+
 	client := clHttpClient.NewClient("https://api.weixin.qq.com/sns/userinfo")
-	client.AddParam("access_token", accessToken)
-	client.AddParam("openid", _openId)
+	client.AddParam("access_token", accessTokenInfo.AccessToken)
+	client.AddParam("openid", accessTokenInfo.OpenId)
 	client.AddParam("lang", "zh_CN")
 
 	res, err := client.Do()
